fdd31081b756157550127b39b7919a0b7f221766
jayamohanan96 <jayamohanan96@gmail.com>
2025-10-16 06:20:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+// Draw a rectangle
+ctx.fillStyle = "red";
+ctx.fillRect(50, 50, 100, 100);
+
+console.log("Game started!");
